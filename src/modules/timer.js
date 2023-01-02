2f6a7c9d37dde8d225fe5ea6a5be445b02f5df86
Tevikansky <valerabogdanov@inbox.ru>
2023-01-02 21:45:08 +0600

    добавлены файлы

---
+++ src/modules/timer.js
@@ -0,0 +1,66 @@
+const timer = (deadline) => {
+  const timerHours = document.getElementById('timer-hours')
+  const timerMinutes = document.getElementById('timer-minutes')
+  const timerSeconds = document.getElementById('timer-seconds')
+
+  const getTimeRemaining = () => {
+    let dataStop = new Date(deadline).getTime();
+    let dataNow = new Date().getTime();
+    let timeRemaining = (dataStop - dataNow) / 1000;
+    // let days = Math.floor((timeRemaining / 86400));
+    let hours = Math.floor((timeRemaining / 3600) % 3600);
+    let minutes = Math.floor((timeRemaining / 60) % 60);
+    let seconds = Math.floor(timeRemaining % 60);
+
+    if (hours < 10) {
+      hours = "0" + Math.floor((timeRemaining / 3600) % 3600);
+    }
+    if (minutes < 10) {
+      minutes = "0" + Math.floor((timeRemaining / 60) % 60);
+    }
+    if (seconds < 10) {
+      seconds = "0" + Math.floor(timeRemaining % 60);
+    }
+    if (timeRemaining < 0) {
+      hours = "00";
+      minutes = "00";
+      seconds = "00";
+      clearInterval(interval);
+    }
+
+    return {
+      timeRemaining,
+      hours,
+      minutes,
+      seconds
+    };
+
+
+  };
+
+  const updateClock = () => {
+    let getTime = getTimeRemaining();
+    timerHours.textContent = getTime.hours;
+    timerMinutes.textContent = getTime.minutes;
+    timerSeconds.textContent = getTime.seconds;
+    console.log("Я секунда")
+  }
+
+  updateClock()
+
+  let getTime = getTimeRemaining();
+  let interval = setInterval(updateClock, 1000)
+  // if (getTime.timeRemaining === 0) {
+  //   clearInterval(interval);
+  //   interval = null;
+  // }
+
+
+
+
+
+
+
+}
+
+export default timer

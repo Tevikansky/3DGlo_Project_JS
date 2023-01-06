afbdbea46ed9a668d7c78432bf86da7d4ac4ba1a
Tevikansky <valerabogdanov@inbox.ru>
2023-01-07 01:09:58 +0600

    homework #25 is comleted: add helper.js

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,37 +1,35 @@
+import { animate } from "./helpers";
 const modal = () => {
   const modal = document.querySelector('.popup')
   const modalWindow = modal.querySelector('.popup-content')
   const buttons = document.querySelectorAll(".popup-btn")
-  let count = 0;
-  let idInterval;
+
 
 
   buttons.forEach(btn => {
     btn.addEventListener("click", () => {
       if (window.screen.width > 768) {
         modalWindow.style.left = 0
+        modalWindow.style.transform = `translateX(-50%)`
         modal.style.display = `block`;
-        animate()
+        animate({
+          duration: 700,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            modalWindow.style.left = progress * 50 + '%';
+          }
+        });
       } else {
-        modalWindow.style.left = 30 + "%"
+        modalWindow.style.transform = `translateX(-50%)`
+        modalWindow.style.left = 50 + "%"
         modal.style.display = `block`;
       }
     })
   })
 
-  const animate = () => {
 
-    count++
-    idInterval = requestAnimationFrame(animate)
-
-    if (count < 40) {
-      modalWindow.style.left = count + "%"
-    } else {
-      cancelAnimationFrame(idInterval)
-      count = 0
-    }
-
-  }
 
 
   modal.addEventListener('click', (e) => {

6645ab05a4456958a83bc718875d189f26b0580c
Tevikansky <valerabogdanov@inbox.ru>
2023-01-06 18:39:37 +0600

    выполнено 22 задание

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -2,6 +2,7 @@ const timer = (deadline) => {
   const timerHours = document.getElementById('timer-hours')
   const timerMinutes = document.getElementById('timer-minutes')
   const timerSeconds = document.getElementById('timer-seconds')
+  let timerId = null;
 
   const getTimeRemaining = () => {
     let dataStop = new Date(deadline).getTime();
@@ -25,6 +26,7 @@ const timer = (deadline) => {
       hours = "00";
       minutes = "00";
       seconds = "00";
+      clearInterval(timerId);
     }
 
     return {
@@ -46,11 +48,8 @@ const timer = (deadline) => {
 
   updateClock()
 
-  let getTime = getTimeRemaining();
-  let interval = setInterval(updateClock, 1000)
-  if (getTime.timeRemaining < 0) {
-    clearInterval(interval);
-  }
+
+  timerId = setInterval(updateClock, 1000)
 
 
 

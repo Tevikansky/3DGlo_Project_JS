aa27872d65a6ffb6f40f2a7f84fa91926f60aa37
Tevikansky <valerabogdanov@inbox.ru>
2023-01-06 16:40:44 +0600

    выполнено начало 23 урока

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import validator from './modules/validator';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
+
 
 timer('6 january 2023');
 menu();
 modal();
 validator();
 tabs();
+slider();

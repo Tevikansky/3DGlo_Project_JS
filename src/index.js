d37b597f9e6e8e90aaeb7518c241246a255a76a0
Tevikansky <valerabogdanov@inbox.ru>
2023-01-14 16:59:26 +0600

    change date of end of discount

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import slider from './modules/slider';
 import calc from './modules/calc';
 import sendForm from './modules/sendForm';
 
-timer('6 january 2023');
+timer('1 april 2023');
 menu();
 modal();
 validator();
